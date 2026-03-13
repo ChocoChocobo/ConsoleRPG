bca97112fcb1d5b3cb07a1a7ca75afcc60ae06e2
Levi_333 <leva.nechaev2008@gmail.com>
2026-03-13 13:18:57 +0300

    ПТПМ ДЗ 06.03.2026

--- ConsoleRPG/character.h
+++ ConsoleRPG/character.h
@@ -38,6 +38,9 @@ struct Characteristics
 	/// <param name="characteristic">Значение характеристики, по которой будет считаться модификатор</param>
 	/// <returns>Возвращает модификатор характеристики</returns>
 	int CountModificator(int characteristic);
+	/// <summary>
+	/// Функция отвечает за вывод значений характеристик.
+	/// </summary>
 	void PrintCharacteristics();
 };
 
@@ -56,7 +59,7 @@ struct VisualsASCII
 };
 
 /// <summary>
-/// Структура отвечает за описательную внешность персонажа
+/// Структура отвечает за хранение описательной внешность персонажа. Нужна была для одной из ДЗ.
 /// </summary>
 struct Appearance
 {
@@ -64,6 +67,9 @@ struct Appearance
 	string hairColor;
 };
 
+/// <summary>
+/// Класс отвечает за все данные персонажа: игрока и врага
+/// </summary>
 class Character
 {
 public:
@@ -90,23 +96,91 @@ public:
 	VisualsASCII visualsASCII;
 
 	Character();
+	/// <summary>
+	/// Специальный конструктор для тех, кто умеет призывать миньёнов
+	/// </summary>
+	/// <param name="_name">Имя</param>
+	/// <param name="_health">Максимальное и стартовое здоровье</param>
+	/// <param name="_damageFace">Количетво граней куба урона</param>
+	/// <param name="_specialCooldown">Время перезарядки специальной атаки (в ходах)</param>
+	/// <param name="_startGold">Стартовое золото</param>
+	/// <param name="_minion">Ссылка на миньёна, которого будет призывать персонаж</param>
+	/// <param name="_uniqueAbilityDifficulty">Хз честно</param>
 	Character(string _name, int _health, int _damageFace, int _specialCooldown, int _startGold, Character& _minion, int _uniqueAbilityDifficulty);
+	/// <summary>
+	/// Конструктор для игрока и обычного врага
+	/// </summary>
+	/// <param name="_name">Имя</param>
+	/// <param name="_health">Максимальное и стартовое здоровье</param>
+	/// <param name="_damageFace">Количетво граней куба урона</param>
+	/// <param name="_specialCooldown">Время перезарядки специальной атаки (в ходах)</param>
+	/// <param name="_startGold">Стартовое золото</param>
 	Character(string _name, int _health, int _damageFace, int _specialCooldown, int _startGold);
 
 	// Вспомогательные функции
+	
+	/// <summary>
+	/// Функция отвечает за вывод информации о персонаже
+	/// </summary>
 	void PrintStatus();
+	/// <summary>
+	/// Вспомогательная функция, отвечающая за вычитание денег у персонажа
+	/// </summary>
+	/// <param name="amount">Сколько вычитать</param>
 	void RemoveGold(int amount);
+	/// <summary>
+	/// Вспомогательная функция, отвечающая за прибавление денег персонажу
+	/// </summary>
+	/// <param name="amount">Сколько денег прибавлять</param>
 	void AddGold(int amount);
+	/// <summary>
+	/// Вспомогательная функция, которая отвечает за покупку предмета
+	/// </summary>
+	/// <param name="cost">Стоимость этого предмета</param>
+	/// <returns>Если успешно возвращает true, иначе false</returns>
 	bool BuyItem(int cost);
+	/// <summary>
+	/// Вспомогательная функция, отвечающая за прибавление текущих хитов персонажу
+	/// </summary>
+	/// <param name="amount">Сколько прибавлять</param>
 	void IncreaseHealth(int amount);
+	/// <summary>
+	/// Вспомогательная функция, отвечающая за отнятие текущих хитов персонажу
+	/// </summary>
+	/// <param name="amount">Сколько отнять</param>
 	void DecreaseHealth(int amount);
+	/// <summary>
+	/// Вспомогательная функция, отвечающая за проверку на побег персонажа от своего врага
+	/// </summary>
+	/// <param name="difficulty">Сложность проверки</param>
+	/// <returns>Возвращает true если удачно, иначе false</returns>
 	bool CheckFleeSuccess(int difficulty);
 
 	// Функции, которые должны быть описаны как состояния
+	
+	/// <summary>
+	/// Функция отвечает за основную атаку персонажа
+	/// </summary>
+	/// <param name="other">Персонаж, по которому атакуют</param>
 	void BasicAttack(Character& other);
+	/// <summary>
+	/// нет.
+	/// </summary>
 	void SpecialAttack();
+	/// <summary>
+	/// Функция отвечает за вывод предметов в инвентаре
+	/// </summary>
 	void ShowInventory();
+	/// <summary>
+	/// Функция отвечает за попытку излечиться. Она тратит healthFlasks
+	/// </summary>
+	/// <param name="difficulty">Сложность излечения</param>
 	void Heal(int difficulty);
+	/// <summary>
+	/// Функция отвечает за попытку сбежать от врага. Чем больше хитов у врага тем больше сложность
+	/// </summary>
+	/// <param name="other">Тот, от кого пытаются сбежать</param>
+	/// <returns>Если успешно возвращает true, иначе false</returns>
 	bool Flee(Character& other);
 };
 
